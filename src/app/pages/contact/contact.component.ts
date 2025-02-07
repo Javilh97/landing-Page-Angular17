e4cc23f9b499982bb57b634743a280936f044556
javi <javier.lhernandez97@gmail.com>
2025-02-07 16:51:45 -0600

    second commit

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -11,20 +11,34 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
 
-  contactForm?: FormGroup;
+  contactForm: FormGroup;
 
   constructor(private fb: FormBuilder){
     this.contactForm = this.fb.group({
-      
-    })
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      userName: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', [Validators.required, Validators.minLength(5)]]
+
+    });
   }
 
   enviar(event: Event){
     event.preventDefault();
+    console.log(this.contactForm.value);
+    
   }
 
   ngOnInit(): void {
     
   }
+
+  hasErrors(controlName: string, errorType: string){
+    return this.contactForm.get(controlName)?.hasError(errorType) && this.contactForm.get(controlName)?.touched;
+  }
+
+  
 
 }
